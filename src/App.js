aa2c38e478bdd056633982a61304b30d06c32b85
unknown <javimorenomontejo@gmail.com>
2021-11-04 18:50:48 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import { Link, Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import Home from './components/Home';
+import About from './components/About';
+import NotFound from './components/NotFound';
+import Profile from './components/Profile';
+import {signIn} from "./components/auth";
+import { useState } from 'react';
+import AuthRoute from './components/AuthRoute';
+import LoginForm from './components/LoginForm';
+import LogoutButton from './components/LogoutButton';
 
 function App() {
+  const [user, setUser] = useState(null);
+  const authenticated = user != null;
+
+  const login = ({ email, password }) => setUser(signIn({email, password}));
+  const logout = () => setUser(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <header>
+            <Link to="/">
+              <button>Home</button>
+            </Link>
+            <Link to="/about">
+              <button>About</button>
+            </Link>
+            <Link to="/profile">
+              <button>Profile</button>
+            </Link>
+            {authenticated ? (
+          <LogoutButton logout={logout} />
+        ) : (
+          <Link to="/login">
+            <button>Login</button>
+          </Link>
+        )}
+
+          </header>
+          <main>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route
+            path="/login"
+            render={props => (
+              <LoginForm authenticated={authenticated} login={login} {...props} />
+            )}
+          />
+          <AuthRoute
+            authenticated={authenticated}
+            path="/profile"
+            render={props => <Profile user={user} {...props} />}
+          />
+            <Route path="/profile" component={Profile} />
+            <Route component={NotFound} />
+            <authRoute
+            authenticated={authenticated}
+            path="/profile"
+            render={props => <Profile user={user} {...props} />}
+          />
+            </Switch>
+          </main>
+    </Router>
   );
 }
 
---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <h2>About</h2>
+            <h4>About of who?</h4>
+        </div>
+    )
+}
+
+export default About
